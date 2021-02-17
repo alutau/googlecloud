688f123fa5715817cd1fefd1951d2e88238c00bc
alutau <alutau561@ieselcaminas.org>
2021-02-17 17:23:44 +0100

    funciona

---
+++ my-nodejs-service/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// [START app]
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require(`path`);
+
+const app = express();
+
+// [START enable_parser]
+app.use(bodyParser.urlencoded({ extended: true }));
+// [END enable_parser]
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+// [START add_display_form]
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+// [END add_display_form]
+
+// [START add_post_handler]
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
+});
+// [END add_post_handler]
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+// [END app]
+
+module.exports = app;
+
